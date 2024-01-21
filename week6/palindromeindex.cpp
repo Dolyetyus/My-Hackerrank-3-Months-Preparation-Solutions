5a01ccfaa97452195b32fa3de2988b5c3d2cdc04
Doli <67073431+Dolyetyus@users.noreply.github.com>
2024-01-21 16:49:34 +0100

    Update palindromeindex.cpp

--- week6/palindromeindex.cpp
+++ week6/palindromeindex.cpp
@@ -5,49 +5,55 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
-
-
 /*
  * Complete the 'palindromeIndex' function below.
  *
  * The function is expected to return an INTEGER.
  * The function accepts STRING s as parameter.
  */
- 
+
+void firstSecond(string &first, string &second, string &s) {
+    string temp = s;
+    if (s.size() % 2 == 1){
+        temp.erase(temp.begin() + (temp.size()/2));
+    }
+    first = temp.substr(0, temp.size() / 2);
+    second = temp.substr(temp.size() / 2, temp.size() / 2);
+    reverse(second.begin(), second.end());
+}
+
 int palindromeIndex(string s) {
-    int ret = -1;
+    unsigned int ret = -1;
     bool is_palin = false;
     
-    cout<<s<<endl;
+    string first;
+    string second;
+    firstSecond(first, second, s);
+        
+    if (first==second){
+        return -1;
+    }
     
-    while (s.size()>1){
-        if (s.size() % 2 == 1){
-            s.erase(s.begin() + (s.size()/2));
-        }
-        cout<<s<<endl;
-        
-        string first = s.substr(0, s.size()/2);
-        string second = s.substr(s.size()/2, s.size()-1);
-        reverse(second.begin(), second.end());
-        
-        if (first==second){
-            break;
-        }
-        
-        cout<<"F:"<<first<<endl<<"S:"<<second<<endl;
-        
-        for (int i = 0; i<first.size(); i++){
-            if (first[i] != second[i]){
-                s.erase(i);
+    for (unsigned int i = 0; i < first.size(); i++){
+        if (first[i] != second[i]){
+            string tempfirst;
+            string tempsecond;
+            string tempS = s;
+            tempS.erase(tempS.begin() + i);
+            
+            firstSecond(tempfirst, tempsecond, tempS);
+            
+            if (tempfirst == tempsecond){
                 ret = i;
-                cout<<"removal happened"<<endl;
-                return i;
             }
+            else{
+                ret = s.size()-1-i;
+            }
+            break;
         }
     }
     
     return ret;
-
 }
 
 int main()
